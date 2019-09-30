3a7ec138505c20e51383cd20082f8be990c7cb27
anthropo5 <rk606797@gmail.com>
2019-09-30 09:41:34 +0200

    add redux-form template

--- src/components/DishForm/DishForm.js
+++ src/components/DishForm/DishForm.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const DishForm = () => {
-    return (
-        <form>
-            <input type="text"/>
-        </form>
-    )
-}
+const submit = values => {
+    console.log(values)
+  }
 
-export default DishForm
+let DishForm = props => {
+  const { handleSubmit } = props;
+
+  return (
+    <form onSubmit={handleSubmit(submit)}>
+      <div>
+        <label htmlFor="firstName">First Name</label>
+        <Field name="firstName" component="input" type="text" />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name</label>
+        <Field name="lastName" component="input" type="text" />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <Field name="email" component="input" type="email" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+DishForm = reduxForm({
+  form: "dish"
+})(DishForm);
+
+export default DishForm;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { reducer as formReducer } from "redux-form";
+
+const rootReducer = combineReducers({
+  form: formReducer
+});
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
