6b6825eac1af1862b01707c6810ea790af9984c7
anthropo5 <rk606797@gmail.com>
2019-09-30 10:16:00 +0200

    add select dish type field. get dish-type from redux-form state

--- src/components/DishForm/DishForm.js
+++ src/components/DishForm/DishForm.js
@@ -1,61 +1,83 @@
 import React from "react";
-import { Field, reduxForm, SubmissionError } from "redux-form";
+import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
+import { SubmissionError, formValueSelector } from "redux-form";
 
-
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 function submit(values) {
   return sleep(1000).then(() => {
-      console.log(values);
-      
-      throw new SubmissionError({
-        username: 'User does not exist',
-        _error: 'Login failed!'
-      })
-  })
+    console.log(values);
+
+    throw new SubmissionError({
+      username: "User does not exist",
+      _error: "Login failed!"
+    });
+  });
 }
 
 const renderField = ({ input, label, type, meta: { touched, error } }) => (
+  <div>
+    <label>{label}</label>
     <div>
-      <label>{label}</label>
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <input {...input} placeholder={label} type={type} />
+      {touched && error && <span>{error}</span>}
     </div>
-  )
+  </div>
+);
 
 let DishForm = props => {
-    const { error, handleSubmit, pristine, reset, submitting } = props
-    return (
-      <form onSubmit={handleSubmit(submit)}>
-        <Field
-          name="username"
-          type="text"
-          component={renderField}
-          label="Username"
-        />
-        <Field
-          name="password"
-          type="password"
-          component={renderField}
-          label="Password"
-        />
-        {error && <strong>{error}</strong>}
+  const { error, handleSubmit, pristine, reset, submitting, dishType } = props;
+  return (
+    <form onSubmit={handleSubmit(submit)}>
+      <Field name="name" type="text" component={renderField} label="Name" />
+      <Field
+        name="preparation_time"
+        type="text"
+        component={renderField}
+        label="Preparation time"
+      />
+
+      <div>
+        <label>Dish type</label>
         <div>
-          <button type="submit" disabled={submitting}>
-            Log In
-          </button>
-          <button type="button" disabled={pristine || submitting} onClick={reset}>
-            Clear Values
-          </button>
+          <Field name="type" component="select">
+            <option value="pizza">Pizza</option>
+            <option value="soup">Soup</option>
+            <option value="sandwich">Sandwich</option>
+          </Field>
         </div>
-      </form>
-    )
+      </div>
+
+      <p>type: {dishType} </p>
+
+      {error && <strong>{error}</strong>}
+      <div>
+        <button type="submit" disabled={submitting}>
+          Log In
+        </button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>
+          Clear Values
+        </button>
+      </div>
+    </form>
+  );
 };
 
+const selector = formValueSelector("dish");
+
 DishForm = reduxForm({
-  form: "dish"
+  form: "dish",
+  initialValues: {
+    type: "pizza"
+  }
+})(DishForm);
+
+DishForm = connect(state => {
+  const type = selector(state, "type");
+  return {
+    dishType: type
+  };
 })(DishForm);
 
 export default DishForm;
